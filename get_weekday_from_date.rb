e3054e041992857f35cbeff9f167a9bad3aa9e7e
Jim Farrugia <the.jim.farrugia@gmail.com>
2019-02-23 19:39:24 +1100

    moved get_century_code outside of main method, added method for calculating leap years, added comments and notes

--- get_weekday_from_date.rb
+++ get_weekday_from_date.rb
@@ -1,16 +1,57 @@
-# THIS METHOD ASSUMES THE GIVEN 'DATE' ARGUMENT IS A STRING IN THE FOLLOWING FORMAT "YYYY MM DD"
+# @method:  get_century_code
+# @desc:    match the given century to a value in the century_codes hash/table.
+# @inputs:  century : int : expects two digits
+# @output:  century_code : int
+def get_century_code century
+  century_codes = {
+    17 => 4, # 1700s
+    18 => 2, # 1800s
+    19 => 0, # 1900s
+    20 => 6  # 2000s
+    # Gregorian calendar repeats every four hundred years
+  }
+  i = century
+  if century < 17
+    i += 4 until i >= 17
+    century_code = century_codes[i]
+  elsif century > 20
+    i -= 4 until i <= 20
+    century_code = century_codes[i]
+  else
+    century_code = century_codes[i]
+  end
+end
 
+# @method:  is_leap_year
+# @desc:    calculate whether the given year is a leap year or not
+# @inputs:  date : string
+# @output:  boolean
+def is_leap_year date
+  year = date[0..3].to_i
+  if (year % 4) != 0
+    # leap years are all perfectly divisible by four
+    false
+  elsif ((year % 100) == 0) and ((year % 400) != 0)
+    # years that are divisible by 100 and not by 400 are not leap years
+    false
+  else
+    true
+  end
+end
+
+#
 def get_weekday_from_date date
+
   # Using 'The Key Value Method' for getting the weekday of any date (in the Gregorian calendar)
   # which I found at http://mathforum.org/dr.math/faq/faq.calendar.html
 
-  weekday = nil # will act as running total, then result string
+  weekday = nil
   century = date[0..1].to_i # first two digits of the year
-  year    = date[2..3].to_i #  last two digits of the year
+  year    = date[2..3].to_i # last two digits of the year
   month   = date[5..6].to_i
   day     = date[8..9].to_i
 
-  month_codes = {
+  month_values = {
     1  => 1, # Jan
     2  => 4, # Feb
     3  => 4, # Mar
@@ -25,44 +66,19 @@ def get_weekday_from_date date
     12 => 6  # Dec
   }
 
-  def get_century_code century
-    century_codes = {
-      17 => 4, # 1700s
-      18 => 2, # 1800s
-      19 => 0, # 1900s
-      20 => 6  # 2000s
-      # Gregorian calendar repeats every four hundred years
-    }
-    i = century
-    if century < 17
-      i += 4 until i >= 17
-      code = century_codes[i]
-    elsif century > 20
-      i -= 4 until i <= 20
-      code = century_codes[i]
-    else
-      code = century_codes[i]
-    end
-  end # of get_century_code century
-
-  # Take the last two digits of the year
-  # Divide by 4 and drop any remainder
+  # Divide the last two digits of the year by 4
   weekday = year / 4
-
   # Add the day of the month.
   weekday += day
-
   # Add the month's key value
-  weekday += month_codes[month]
-
+  weekday += month_values[month]
   # If the date is in January or February of a leap year, subtract 1.
+  
 
   # Add the century code
   weekday += get_century_code century
-
   # Add the last two digits of the year
   weekday += year
-
   # Divide by 7 and take the remainder
   weekday = weekday % 7
 
@@ -87,3 +103,22 @@ date = gets.chomp
 # Output result to user
 weekday = get_weekday_from_date date
 puts "#{date} will be a #{weekday}."
+
+
+# THIS METHOD ASSUMES THE GIVEN 'DATE' ARGUMENT IS A STRING IN THE FOLLOWING FORMAT "YYYY MM DD"
+
+=begin
+A note about calculating leap years (from timeanddate.com/date/leapyear.html):
+
+  We add a Leap Day on February 29, almost every four years.
+
+  In the Gregorian calendar three criteria must be taken into account to identify leap years:
+
+  * The year can be evenly divided by 4;
+  * If the year can be evenly divided by 100, it is NOT a leap year, unless;
+  * The year is also evenly divisible by 400. Then it is a leap year.
+  
+  This means that in the Gregorian calendar, the years 2000 and 2400 are leap years, 
+  while 1800, 1900, 2100, 2200, 2300 and 2500 are NOT leap years.
+
+=end
